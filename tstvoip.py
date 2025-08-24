5e9356439ae050b8217ebe172d25d24b2d736f0b
dim5x <dim5x@yahoo.com>
2025-08-24 23:38:12 +0300

    16

--- tstvoip.py
+++ tstvoip.py
@@ -394,8 +394,8 @@ async def main():
     # Добавляем маршруты
     app.router.add_get('/', index_handler)
     app.router.add_get('/ws', server.websocket_handler)
-    app.router.add_get('/static/{filename}', js_handler)
-    app.router.add_get('/static/{filename}', css_handler)
+    app.router.add_get('/static/client.js', js_handler)
+    app.router.add_get('/static/style.css', css_handler)
     app.router.add_options('/ws', lambda request: web.Response(status=200))  # для CORS
 
     runner = web.AppRunner(app)
